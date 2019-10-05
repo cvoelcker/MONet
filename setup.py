18ccfb843a8fff6061b02f87d208040597b8df08
cvoelcker (iliricon) <claas@voelcker.net>
2019-10-05 16:33:08 +0200

    Fixed setup to include utils

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='Spatial MONet',
       version='0.2',
       description='Implementation of MONet with spatial transformers',
       author='Claas Voelcker',
       author_email='claas@voelcker.net',
-      py_modules=['spatial_monet.spatial_monet', 'spatial_monet.model', 'spatial_monet.experiment_config'],
+      packages=find_packages(),
       install_requires=['numpy', 'torch', 'torchvision']
      )
